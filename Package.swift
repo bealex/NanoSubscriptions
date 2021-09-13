c93af16612be8960869b6f2bac5de7a809b1ec3d
Alex Babaev <a.babaev@redmadrobot.com>
2021-09-13 12:40:29 +0300

    Fixed package name

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Memoirs",
+    name: "NanoSubscriptions",
     platforms: [ .iOS(.v11), .macOS(.v11) ],
     products: [
         .library(name: "NanoSubscriptions", type: .static, targets: [ "NanoSubscriptions" ]),
